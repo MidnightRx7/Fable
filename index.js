c88f6f3645215ea610cc596ae11d356ed5a32155
Daniel <danielegutierrez99@gmail.com>
2024-11-02 18:07:21 -0700

    new

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
 const express = require('express')
+
 const app = express()
-const port = 80
 
-app.get('/', (req, res) => {
-  res.send('Fable')
+app.use(express.static('Fable'))
+
+app.listen(3000, () => {
+    console.log("App is listening on port 3000")
 })
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+// const port = 80
+
+// app.get('/', (req, res) => {
+//   res.send('Fable')
+// })
+
+// app.listen(port, () => {
+//   console.log(`Example app listening on port ${port}`)
+// })
